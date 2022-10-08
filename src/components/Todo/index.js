1a4bba999d72e0f05dd284015b42acf8e1d05439
olo90632951 <olo90632951@gmail.com>
2022-10-09 01:56:47 +0900

    Feat: createTodo API 이용해서 일정 추가 로직 구현 [Assignment4-2]

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,20 +1,33 @@
 import { useGetTodos } from "../../hooks/useGetTodos";
+import { useTodo } from "../../hooks/useTodo";
 
 const Todo = () => {
   const todos = useGetTodos();
+  const { content, onSubmitTodo } = useTodo();
 
   return (
-    <ul>
-      {todos &&
-        todos.map((item) => (
-          <li key={item.id}>
-            <span>{item.id}</span>
-            <span>{item.todo}</span>
-            <span>{item.isCompleted ? "true" : "false"}</span>
-            <span>{item.userId}</span>
-          </li>
-        ))}
-    </ul>
+    <>
+      <form onSubmit={onSubmitTodo}>
+        <input
+          type="text"
+          placeholder="내용을 입력해주세요."
+          value={content.value}
+          onChange={content.onChangeInput}
+        />
+        <button type="submit">추가</button>
+      </form>
+      <ul>
+        {todos &&
+          todos.map((item) => (
+            <li key={item.id}>
+              <span>{item.id}</span>
+              <span>{item.todo}</span>
+              <span>{item.isCompleted ? "true" : "false"}</span>
+              <span>{item.userId}</span>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 };
 
--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,1 +1,13 @@
-export const useTodo = async () => {};
+import { createTodo } from "../apis/todo";
+import { useInput } from "./useInput";
+
+export const useTodo = () => {
+  const content = useInput("");
+
+  const onSubmitTodo = (e) => {
+    e.preventDefault();
+    createTodo({ todo: content.value });
+  };
+
+  return { content, onSubmitTodo };
+};
